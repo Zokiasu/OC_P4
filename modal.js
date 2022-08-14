f13e49c0644ba373bc5871a4ea0ac366a790526b
Zokiasu <pierrick.tly@gmail.com>
2022-08-14 18:54:59 +0200

    fix check radio and check birthdate

--- modal.js
+++ modal.js
@@ -42,13 +42,13 @@ const email = document.querySelector('#email');
 const birthdate = document.querySelector('#birthdate');
 const quantity = document.querySelector('#quantity');
 const conditionsOfUse = document.querySelector('#conditionsOfUse');
-const locations = document.querySelector("#locations");
+const radio = document.querySelectorAll('input[type="radio"]');
 
 // Error messages
 const ErrFirst = "Le champ prénom a un minimum de 2 caractères";
 const ErrLast = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
 const ErrEmail = "L'adresse électronique n'est pas valide.";
-const ErrBirthdate = "Vous devez entrer votre date de naissance.";
+const ErrBirthdate = "Vous devez entrer une date de naissance valide.";
 const ErrQuantity ="Vous devez entrer votre nombre de participation.";
 const ErrLocations ="Vous devez choisir une ville.";
 const ErrConditionofUse = "Vous devez vérifier que vous acceptez les conditions.";
@@ -82,14 +82,59 @@ const setSuccess = element => {
   inputControl.classList.remove('error');
 };
 
+//check if one radio button is checked
+const checkRadio = () => {  
+  let checked = false;
+  radio.forEach(radio => {
+    if (radio.checked) {
+      checked = true;
+    }
+  });
+
+  return checked;
+}
+
+//check if birthdate is valid
+const checkBirthdate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+
+  const birth = birthdate.value;
+  const birthdateArray = birth.split('-');
+  const birthdateYear = birthdateArray[0];
+  const birthdateMonth = birthdateArray[1];
+  const birthdateDay = birthdateArray[2];
+
+  if (birthdateYear > year) {
+    return true;
+  } else if (birthdateYear == year) {
+    if (birthdateMonth > month) {
+      return true;
+    } else if (birthdateMonth == month) {
+      if (birthdateDay > day) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+
 const validateInputs = () => {
   first.value.trim() === '' ? setError(first, ErrFirst) : setSuccess(first);
   last.value.trim() === '' ? setError(last, ErrLast) : setSuccess(last);
   email.value.trim() === '' ? setError(email, ErrEmail) : setSuccess(email);
-  birthdate.value.trim() === '' ? setError(birthdate, ErrBirthdate) : setSuccess(birthdate);
+  checkBirthdate() || birthdate.value.trim() === '' ? setError(birthdate, ErrBirthdate) : setSuccess(birthdate);
   quantity.value.trim() === '' ? setError(quantity, ErrQuantity) : setSuccess(quantity);
   conditionsOfUse.checked === false ? setError(conditionsOfUse, ErrConditionofUse) : setSuccess(conditionsOfUse);
-  locations.value === '' ? setError(locations, ErrLocations) : setSuccess(locations);
+  !checkRadio() ? setError(locations, ErrLocations) : setSuccess(locations);
 
   if(first.value.trim() !== '' && last.value.trim() !== '' && email.value.trim() !== '' && birthdate.value.trim() !== '' && quantity.value.trim() !== '' && conditionsOfUse.checked === true && locations.value !== ''){
     launchFormResponse();
